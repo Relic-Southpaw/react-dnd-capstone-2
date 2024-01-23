25bff3ac61757d949bf8210bc4ea86d2fdc46d9d
Relic <RobertSmithTheFourth@Outlook.com>
2024-01-22 21:45:32 -0500

    tracing errorsgit add .!

--- frontend/src/context/UserContextProvider.js
+++ frontend/src/context/UserContextProvider.js
@@ -33,6 +33,7 @@ function UserContextProvider({ children }) {
         try {
             let token = await DnDApi.register(signUpData)
             setToken(token)
+            console.log(token)
             navigate(`/login`)
             return { success: true }
         } catch (err) {
